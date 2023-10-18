48eb4edaa03849d136a8456b48294ab77f89acb7
Sheila Chebii <shillerchier055@gmail.com>
2023-10-19 00:08:20 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let form=document.querySelector('form');
+form=addEventListener('submit',(e)=>{
+e.preventDefault();
+toDoo(e.target.new_task_description.value)
+//form.reset()->made to clear after entering the data,it giving errors tho;
+
+})
+ })
+
+function toDoo(toDo){
+let lis=document.createElement('li');
+lis.textContent=`${toDo}`;
+document.querySelector('#list').appendChild(lis)
+let del=document.createElement('button');
+del.textContent='X';
+lis.appendChild(del);
+del.addEventListener('click',handleDelete)
+
+}
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
